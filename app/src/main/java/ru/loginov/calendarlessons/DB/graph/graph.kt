76f6c8a8e1f004ec0c0d97ecf54c949fa33eafcb
LginvM <84954335+LginvM@users.noreply.github.com>
2026-03-25 00:00:30 +0600

    graph

---
+++ app/src/main/java/ru/loginov/calendarlessons/DB/graph/graph.kt
@@ -0,0 +1,22 @@
+package ru.loginov.calendarlessons.DB.graph
+
+import android.content.Context
+import ru.loginov.calendarlessons.DB.database.lessonDatabase
+import ru.loginov.calendarlessons.DB.repository.Repository
+
+object graph {
+    lateinit var db: lessonDatabase
+        private set
+
+    val repository by lazy {
+        Repository(
+            userDao = db.userDao(),
+            typeLessonDao = db.typeDao(),
+            lessonDao = db.lessonDao(),
+        )
+    }
+
+    fun provide(context: Context){
+        db = lessonDatabase.getDatabase(context)
+    }
+}
