e54f26a548e0799a034dcf0a3d963e5019d86be3
LginvM <84954335+LginvM@users.noreply.github.com>
2026-03-31 20:25:57 +0600

    maybe it will work)

--- app/src/main/java/ru/loginov/calendarlessons/Screens/allAboutUser/screenUserInfo.kt
+++ app/src/main/java/ru/loginov/calendarlessons/Screens/allAboutUser/screenUserInfo.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,13 +13,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ru.loginov.calendarlessons.ViewModels.DetailState
@@ -43,6 +47,7 @@ fun UserInfo(
             onPassword = viewModel::onPassword,
             onBirthday = viewModel::onBirthday,
             onBalance = viewModel::onBalance,
+            onNotice = viewModel::onNotice,
         ){
             navigateUp.invoke()
         }
@@ -59,9 +64,13 @@ private fun InfoDetailEntry(
     onPassword: (String) -> Unit,
     onBirthday: (String) -> Unit,
     onBalance: (Int) -> Unit,
+    onNotice: (String) -> Unit,
     navigateUp: () -> Unit,
+
 ){
-    var isNewEnabled by remember{
+    var textStr by rememberSaveable { mutableStateOf("") }
+    
+    var isFocused by remember{
         mutableStateOf(false)
     }
 
@@ -139,9 +148,16 @@ private fun InfoDetailEntry(
         Spacer(modifier= Modifier.Companion.size(12.dp))
 
 
+        LaunchedEffect(key1 = state.balance,isFocused){
+            if(!isFocused){
+                textStr = state.balance.let { String.format("%d") }
+            }
+        }
+
+
         TextField(
-            value = state.balance,
-            onValueChange = { onBalance(it) },
+            value = textStr,
+            onValueChange = { newText -> textStr.filter { it.isDigit() } },
             label = {
                 Text(text = "Баланс")
             },
@@ -155,12 +171,12 @@ private fun InfoDetailEntry(
         Spacer(modifier= Modifier.Companion.size(12.dp))
 
         TextField(
-            value = state.birthday,
-            onValueChange = { onBirthday(it) },
+            value = state.notice,
+            onValueChange = { onNotice(it) },
             label = {
-                Text(text = "Номер")
+                Text(text = "Заметки")
             },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().height(260.dp),
             colors = TextFieldDefaults.colors(
                 unfocusedIndicatorColor = Color.LightGray
             ),
@@ -169,4 +185,27 @@ private fun InfoDetailEntry(
         Spacer(modifier= Modifier.Companion.size(12.dp))
 
     }
+}
+
+
+
+fun intConv(input:String):Int{
+    val normalized = input.filter { it.isDigit() }
+    return normalized.toInt()
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun PrevDetailEntry(){
+    InfoDetailEntry(
+        state = DetailState(),
+        onNameChange = { },
+        onLastNameChange = { },
+        onNumber = { },
+        onPassword = { },
+        onBirthday = { },
+        navigateUp = {},
+        onBalance = { },
+        onNotice = { },
+    )
 }
